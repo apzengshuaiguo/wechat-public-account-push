cbc1bebe780c1ba17e01d0350c03b32dd031e13b
apzengshuaiguo <67819163+apzengshuaiguo@users.noreply.github.com>
2024-01-26 10:52:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '姐姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT237745TgIrVVqz4WydBg1ksUovikSaw',
+      id: '21d5610fd7e14107b99ecdd8fbe488d9',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
